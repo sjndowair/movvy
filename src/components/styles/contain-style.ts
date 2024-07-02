81f7e5c2143e01e9c748b2b5aef5aee3acbe9f46
sjndowair <wodnjsdl1125@naver.com>
2024-07-03 01:12:19 +0900

    feat: 이미지 한곳에 모으기

---
+++ src/components/styles/contain-style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PictureBox = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+`;
+export const Overlay = styled.div`
+  position: relative;
+`;
+export const ContainImg = styled.img`
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+`;
+export const Banner = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
