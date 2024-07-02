75df1393aae258272a9b5e02a3159d7d6b8e0755
sjndowair <wodnjsdl1125@naver.com>
2024-07-03 02:04:07 +0900

    add: 슬라이드 임시 스타일 지정

--- src/components/styles/contain-style.ts
+++ src/components/styles/contain-style.ts
@@ -1,20 +1,43 @@
 import styled from "styled-components";
 
-export const PictureBox = styled.div`
-  position: relative;
-  width: 100%;
-  height: auto;
-`;
-export const Overlay = styled.div`
-  position: relative;
-`;
-export const ContainImg = styled.img`
-  width: 100%;
-  height: auto;
-  overflow: hidden;
-`;
-export const Banner = styled.img`
+export const Slider = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
+  height: auto;
+  width: 100%;
+`;
+
+export const Slides = styled.div`
+  display: flex;
+  transition: transform 0.5s ease-in-out;
+`;
+
+export const Overlay = styled.div`
+  min-width: 100%;
+  box-sizing: border-box;
+`;
+
+export const NextBtn = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0, 5);
+  color: #fff;
+  border: none;
+  padding: 0.75rem;
+  cursor: pointer;
+  z-index: 2;
+  left: 0.75rem;
+`;
+export const PrevBtn = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0, 5);
+  color: #fff;
+  border: none;
+  padding: 0.75rem;
+  cursor: pointer;
+  z-index: 2;
+  right: 0.75rem;
+  color: #000;
 `;
