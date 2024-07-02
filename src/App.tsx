e71fe32cc56cacc1eca6a59673be6ead3b975abb
sjndowair <wodnjsdl1125@naver.com>
2024-07-03 01:12:44 +0900

    fix: 컴포넌트 재생성으로 인한 삭제 후 재입력

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Layout from "./components/layouts/Layout";
 import { darkTheme, lightTheme } from "./components/styles/theme";
 import { GlobalStyle } from "./components/styles/global-style";
 import { useThemeMode } from "./components/Context/themeCtx";
-import PictureArea from "./components/layouts/PictureArea";
+import PictureArea from "./components/layouts/Picture";
 
 function App() {
   const { isDark } = useThemeMode();
