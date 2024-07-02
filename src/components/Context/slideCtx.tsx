9a97bbf4ea6498ba2297a2b30688a5026b929200
sjndowair <wodnjsdl1125@naver.com>
2024-07-03 02:03:13 +0900

    feat: 슬라이드 더미 데이터 저장

---
+++ src/components/Context/slideCtx.tsx
@@ -0,0 +1,52 @@
+import { Slide } from "../styles/slick-type";
+
+export const slideData: Slide[] = [
+  {
+    webSrcSet:
+      "https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/20240508_banner_135210.webp 1x, https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.webp 1.7x, https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/20240508_banner_135210.webp 2.5x",
+    imgSrc:
+      "https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.jpg",
+    alt: "일드_25시, 아카사카에서",
+    isBanner: false,
+  },
+  {
+    webSrcSet:
+      "https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/20240508_banner_135210.webp 1x, https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.webp 1.7x, https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/20240508_banner_135210.webp 2.5x",
+    imgSrc:
+      "https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.jpg",
+    alt: "일드_25시, 아카사카에서",
+    isBanner: false,
+  },
+  {
+    webSrcSet:
+      "https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/20240508_banner_135210.webp 1x, https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.webp 1.7x, https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/20240508_banner_135210.webp 2.5x",
+    imgSrc:
+      "https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.jpg",
+    alt: "일드_25시, 아카사카에서",
+    isBanner: false,
+  },
+  {
+    webSrcSet:
+      "https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/20240508_banner_135210.webp 1x, https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.webp 1.7x, https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/20240508_banner_135210.webp 2.5x",
+    imgSrc:
+      "https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.jpg",
+    alt: "일드_25시, 아카사카에서",
+    isBanner: false,
+  },
+  {
+    webSrcSet:
+      "https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/20240508_banner_135210.webp 1x, https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.webp 1.7x, https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/20240508_banner_135210.webp 2.5x",
+    imgSrc:
+      "https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.jpg",
+    alt: "일드_25시, 아카사카에서",
+    isBanner: false,
+  },
+  {
+    webSrcSet:
+      "https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/20240508_banner_135210.webp 1x, https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.webp 1.7x, https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/20240508_banner_135210.webp 2.5x",
+    imgSrc:
+      "https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.jpg",
+    alt: "일드_25시, 아카사카에서",
+    isBanner: false,
+  },
+];
