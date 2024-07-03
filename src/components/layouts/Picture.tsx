785a786834b46a662b49cdff51d2149543afd9cc
sjndowair <wodnjsdl1125@naver.com>
2024-07-03 12:38:54 +0900

    add: 슬라이드 구현

--- src/components/layouts/Picture.tsx
+++ src/components/layouts/Picture.tsx
@@ -1,51 +1,45 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useState } from "react";
 import {
-  PictureBox,
+  Slider,
+  Slides,
   Overlay,
-  ContainImg,
-  Banner,
+  NextBtn,
+  PrevBtn,
 } from "../styles/contain-style";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { slideData } from "../Context/slideCtx";
+
+use;
 
 const PictureArea = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  const [slide, setSlide] = useState<number>(0);
+
+  const nextSlide = useCallback((): void => {
+    setSlide((prev) => (prev + 1) % slideData.length);
+  }, []);
+  const prevSlide = useCallback((): void => {
+    setSlide((next) => (next - 1 + slideData.length) % slideData.length);
+  }, []);
+
   return (
-    <PictureBox>
-      <Slider {...settings}>
-        {[...Array(3)].map((_, index) => (
+    <Slider>
+      <Slides style={{ transform: `translateX(${-slide * 100}%)` }}>
+        {slideData.map((slideItem, index) => (
           <Overlay key={index}>
             <picture>
-              <source
-                srcSet="https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/20240508_banner_135210.webp 1x,https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.webp 1.7x,https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/20240508_banner_135210.webp 2.5x"
-                type="image/webp"
-              />
-              <ContainImg
-                src="https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/20240508_banner_135210.jpg"
-                alt="일드_25시, 아카사카에서"
-              />
-            </picture>
-            <picture>
-              <source
-                srcSet="https://image.wavve.com/v1/thumbnails/1240_508_20_80/banner/pooq/2024/imgbuild_20240701_145546523.webp 1x,https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/imgbuild_20240701_145546523.webp 1.7x,https://image.wavve.com/v1/thumbnails/3720_1524_20_80/banner/pooq/2024/imgbuild_20240701_145546523.webp 2.5x"
-                type="image/webp"
-              />
-              <Banner
-                src="https://image.wavve.com/v1/thumbnails/2480_1016_20_80/banner/pooq/2024/imgbuild_20240701_145546523.png"
-                alt="일드_25시, 아카사카에서"
-              />
+              <source srcSet={slideItem.webSrcSet} type="image/webp" />
+              {slideItem.isBanner ? (
+                <img src={slideItem.imgSrc} alt={slideItem.alt} />
+              ) : (
+                <img src={slideItem.imgSrc} alt={slideItem.alt} />
+              )}
             </picture>
           </Overlay>
         ))}
-      </Slider>
-    </PictureBox>
+      </Slides>
+      <PrevBtn onClick={prevSlide}>전</PrevBtn>
+      <NextBtn onClick={nextSlide}>후</NextBtn>
+    </Slider>
   );
 };
 
