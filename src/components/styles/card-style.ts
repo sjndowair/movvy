fcf0f81250029e6184b89b1db88be72e1cf73fb1
sjndowair <wodnjsdl1125@naver.com>
2024-07-06 01:50:28 +0900

    feat: cardComponet 스타일컴포넌트 분할

---
+++ src/components/styles/card-style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  overflow: hidden;
+  padding-top: 2.25rem;
+`;
+
+export const BoardCard = styled.img`
+  border-radius: 20px;
+`;
+
+export const Button = styled.button`
+  background-color: blueviolet;
+`;
