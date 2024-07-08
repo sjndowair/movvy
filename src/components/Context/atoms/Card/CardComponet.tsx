62287073a6dd8048ca32c98b871954cc65812f0b
sjndowair <wodnjsdl1125@naver.com>
2024-07-09 00:27:26 +0900

    fix: 카드 컴포넌트 위치 변경

--- src/components/Context/atoms/Card/CardComponet.tsx
+++ src/components/Context/atoms/Card/CardComponet.tsx
@@ -1,3 +1,21 @@
+import React, { useCallback, useState, useEffect } from "react";
+import { cardData } from "../../../../DummyData/cardData";
+import { H2 } from "../Slider/contain-style";
+import {
+  CardContainer,
+  Button,
+  Slide,
+  SlideWrapper,
+  Card,
+  Prev,
+  Coordinates,
+  Next,
+  More,
+  Intro,
+} from "./card-style";
+
+interface CardComponentProps {
+  title: string;
 }
 
 const CardComponent: React.FC<CardComponentProps> = ({ title }) => {
@@ -26,6 +44,24 @@ const CardComponent: React.FC<CardComponentProps> = ({ title }) => {
     };
   }, []);
 
+  const totalSlides: number = Math.ceil(cardData.length / cardsPerSlide);
+
+  const prevSlide = useCallback(() => {
+    setSlideIndex((prevIndex) =>
+      prevIndex === 0 ? totalSlides - 1 : prevIndex - 1
+    );
+  }, [totalSlides]);
+
+  const nextSlide = useCallback(() => {
+    setSlideIndex((prevIndex) => {
+      if (prevIndex === totalSlides - 1) {
+        return 0; // Reset to the 0th index
+      } else {
+        return prevIndex + 1;
+      }
+    });
+  }, [totalSlides]);
+
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
@@ -34,3 +70,44 @@ const CardComponent: React.FC<CardComponentProps> = ({ title }) => {
     return () => clearInterval(interval);
   }, [nextSlide]);
 
+  return (
+    <section>
+      <Coordinates>
+        <SlideWrapper>
+          <Intro>
+            <H2>{title}</H2>
+            <More>더보기</More>
+          </Intro>
+          <CardContainer
+            className="card-container"
+            slideIndex={slideIndex}
+            totalSlides={totalSlides}
+          >
+            {cardData.map((card, index) => (
+              <Slide key={index}>
+                {card.ImageContain.map((image, imgIndex) => (
+                  <picture key={imgIndex}>
+                    <source srcSet={image.webSrcSet} type="image/webp" />
+                    <Card src={image.imgSrc} alt={image.alt} />
+                  </picture>
+                ))}
+              </Slide>
+            ))}
+          </CardContainer>
+        </SlideWrapper>
+        <Prev
+          onClick={() => {
+            prevSlide();
+          }}
+        ></Prev>
+        <Next
+          onClick={() => {
+            nextSlide();
+          }}
+        ></Next>
+      </Coordinates>
+    </section>
+  );
+};
+
+export default CardComponent;
