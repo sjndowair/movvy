1ec4acb8728fba0a62f55edcb6d4259cb6676937
sjndowair <wodnjsdl1125@naver.com>
2024-07-09 00:26:32 +0900

    add: 카드 컴포넌트 반응형 대응을 위한 코드 추가

---
+++ src/components/Context/atoms/Card/CardComponet.tsx
@@ -0,0 +1,27 @@
+}
+
+const CardComponent: React.FC<CardComponentProps> = ({ title }) => {
+  const [slideIndex, setSlideIndex] = useState<number>(0);
+
+  const calculateCardsPerSlide = () => {
+    if (window.innerWidth >= 768) {
+      return 5;
+    } else if (window.innerWidth >= 480) {
+      return 3;
+    } else {
+      return 1;
+    }
+  };
+  const [cardsPerSlide, setCardsPerSlide] = useState<number>(
+    calculateCardsPerSlide()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setCardsPerSlide(calculateCardsPerSlide());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
