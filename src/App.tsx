170d67e2dba494c4c5e70d173e8acea1b43d7e73
sjndowair <wodnjsdl1125@naver.com>
2024-07-14 18:15:32 +0900

    feat: api 기능 추가
    외부 api를 불러오기위한 key 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,45 @@ import { darkTheme, lightTheme } from "./components/Theme/theme";
 import { GlobalStyle } from "./components/Theme/global-style";
 import { useThemeMode } from "./contexts/themeCtx";
 import Home from "./pages/root";
+import Layout from "./components/layout";
+import { useEffect, useState } from "react";
+import { API_KEY } from "./constants/api.constant";
 
 function App() {
-    const { isDark } = useThemeMode();
+  const { isDark } = useThemeMode();
+  const [movies, setMovies] = useState();
 
-    return (
-        <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
-            <GlobalStyle />
-            <Home />
-        </ThemeProvider>
-    );
+  useEffect(() => {
+    const getNowPlayingMovieList = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+
+      const result = await response.json();
+
+      // console.log(result);
+      setMovies(result);
+    };
+
+    getNowPlayingMovieList();
+  }, []);
+
+  useEffect(() => {
+    // console.log(movies);
+  }, [movies]);
+
+  return (
+    <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
