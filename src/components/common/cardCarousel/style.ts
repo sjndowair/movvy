a7bcbda2d90ce18e6f3ed2094dc0397de9fb674a
sjndowair <wodnjsdl1125@naver.com>
2024-07-14 18:17:01 +0900

    fix: 카드 컴포넌트 디자인 수정
    카드 컴포넌트 arrow before, after에서 svg 이미지 변경 및 위치 수정 및 크기 수정

--- src/components/common/cardCarousel/style.ts
+++ src/components/common/cardCarousel/style.ts
@@ -1,97 +1,94 @@
 import styled from "styled-components";
 
 interface ICardContainerProps {
-    slideIndex: number;
-    totalSlides: number;
+  slideIndex: number;
+  totalSlides: number;
 }
 
 export const CardContainer = styled.div<ICardContainerProps>`
-    display: flex;
-    margin-top: 3rem;
-    transition: transform 0.5s ease-in-out;
-    transform: ${({ slideIndex, totalSlides }) =>
-        `translateX(-${slideIndex * (100 / Math.ceil(totalSlides / 1))}%)`};
-    width: ${({ totalSlides }) => `${totalSlides * 100}%`};
+  display: flex;
+  margin-top: 3rem;
+  transition: transform 0.5s ease-in-out;
+  transform: ${({ slideIndex, totalSlides }) =>
+    `translateX(-${slideIndex * (100 / Math.ceil(totalSlides / 1))}%)`};
+  width: ${({ totalSlides }) => `${totalSlides * 100}%`};
 `;
 
 export const Coordinates = styled.div`
-    position: relative;
+  background-color: #333;
 `;
 
 export const SlideWrapper = styled.div`
-    overflow: hidden;
-    width: 100%;
-    position: relative;
+  overflow: hidden;
+  position: relative;
+  width: 100%;
 `;
 
 export const Slide = styled.div`
-    box-sizing: border-box;
-    padding: 0 1.2rem;
-    overflow: hidden;
-    cursor: pointer;
+  box-sizing: border-box;
+  padding: 0 1.2rem;
+  overflow: hidden;
+  cursor: pointer;
 `;
 
 export const Card = styled.img`
-    width: 100%;
-    border-radius: 1.25rem;
-    height: auto;
-    object-fit: cover;
-    transition: all 0.2s ease-in;
-    overflow: hidden;
+  width: 100%;
+  border-radius: 1.25rem;
+  height: auto;
+  object-fit: cover;
+  transition: all 0.2s ease-in;
+  overflow: hidden;
 
-    &:hover {
-        scale: 1.02;
-    }
+  &:hover {
+    scale: 1.02;
+  }
 `;
 
 export const Button = styled.button`
-    color: #fff;
+  color: #fff;
 `;
 
 export const Prev = styled.button`
-    position: absolute;
-    left: 2rem;
-    top: 50%;
-    transform: translateY(-50%);
-    &::before {
-        content: "";
-        display: block;
-        width: 20px;
-        height: 20px;
-        border-top: 3px solid ${({ theme }) => theme.color.primary};
-        border-left: 3px solid ${({ theme }) => theme.color.primary};
-        transform: rotate(-45deg);
-        transition: transform 0.3s ease;
-        filter: drop-shadow(0 0 5px rgb(255, 111, 15));
-    }
+  position: absolute;
+  left: 2rem;
+  top: 50%;
+  transform: translateY(50%);
+  width: 50px;
+  height: 50px;
 `;
 
 export const Next = styled.button`
-    position: absolute;
-    right: 2rem;
-    top: 50%;
-    transform: translateY(-50%);
-    &::after {
-        content: "";
-        display: block;
-        width: 20px;
-        height: 20px;
-        border-top: 3px solid ${({ theme }) => theme.color.primary};
-        border-right: 3px solid ${({ theme }) => theme.color.primary};
-        transform: rotate(45deg);
-        transition: transform 0.3s ease;
-        filter: drop-shadow(0 0 5px rgb(255, 111, 15));
-    }
+  position: absolute;
+  top: 50%;
+  right: 2rem;
+  transform: translateY(50%);
+
+  width: 50px;
+  height: 50px;
 `;
 
 export const Intro = styled.div`
-    display: flex;
-    margin-top: 3rem;
-    color: #fff;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  margin-top: 3rem;
+  margin-left: 3rem;
+  width: 90%;
+  font-size: 1.75rem;
+  font-weight: ${({ theme }) => theme.font.bold};
+
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const More = styled.div`
-    font-size: ${({ theme }) => theme.text.xl};
+  font-size: ${({ theme }) => theme.text.xl};
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.125s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.color.primary};
+  }
+`;
+
+export const H2 = styled.h2`
+  color: #fff;
 `;
