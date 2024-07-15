a92e52387fa2b1bbaa8efef6814aae1764850224
sjndowair <wodnjsdl1125@naver.com>
2024-07-16 00:39:57 +0900

    feat: api 요청 주소 추가

---
+++ src/apis/videos.api.ts
@@ -0,0 +1,18 @@
+import {
+  API_KEY,
+  BASE_URL,
+  REQUEST_INIT_OBJECT,
+} from "../constants/api.constant";
+import { IVideosResponse } from "../types/videos";
+
+export const getVideoByMovieId = async (
+  movieId: string | number,
+  reqInit: RequestInit = REQUEST_INIT_OBJECT
+): Promise<IVideosResponse> => {
+  const response = await fetch(
+    `${BASE_URL}/movie/${movieId}/videos?api_key=${API_KEY}&language=ko-KR`,
+    reqInit
+  );
+
+  return await response.json();
+};
