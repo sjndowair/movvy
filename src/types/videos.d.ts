2a543372a00e8a0242724b8ab5bb8b9c1455584a
sjndowair <wodnjsdl1125@naver.com>
2024-07-16 00:34:42 +0900

    feat: movie 및 video 타입 추가

---
+++ src/types/videos.d.ts
@@ -0,0 +1,17 @@
+export interface IVideo {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  official: boolean;
+  published_at: string;
+  site: string;
+  size: number;
+  type: string;
+}
+
+export interface IVideosResponse {
+  id: number;
+  results: IVideo[];
+}
