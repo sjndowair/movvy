1c88b504e7c36fd58f4886ae9be386170efec0de
sjndowair <wodnjsdl1125@naver.com>
2024-07-16 00:39:15 +0900

    feat: footer 메뉴 추가 및 tmdb api 속성 추가

--- src/constants/api.constant.ts
+++ src/constants/api.constant.ts
@@ -1,1 +1,11 @@
-export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_KEY = process.env.REACT_APP_API_KEY; //내가 보유한 고유의 key값이므로 그냥 깃헙에 올리지 않기
+//process.env.REACT_APP_이름
+
+export const BASE_URL = "https://api.themoviedb.org/3";
+
+export const REQUEST_INIT_OBJECT: RequestInit = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+  },
+};
---
+++ src/constants/footer-menus.constant.ts
@@ -0,0 +1,8 @@
+export const FOOTER_MENU_CONTAINER: string[] = [
+  "회사소개",
+  "서비스소개",
+  "이용약관",
+  "개인정보처리방침",
+  "고객센터",
+  "이벤트",
+];
