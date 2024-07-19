1295fbcb301a7c81eb550eead7cb609d278270b1
sjndowair <wodnjsdl1125@naver.com>
2024-07-19 21:12:29 +0900

    fix: 메인 컴포넌트 스타일 수정
    위치이동

--- src/components/layout/style.ts
+++ src/components/layout/style.ts
@@ -3,7 +3,13 @@ import styled from "styled-components";
 interface IMainContainerProps {
   headerToggle: boolean;
 }
-export const MainContainer = styled.main<IMainContainerProps>``;
+export const MainContainer = styled.main<IMainContainerProps>`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  overflow: hidden;
+  height: 100%;
+`;
 
 export const HeaderContainer = styled.header``;
 
