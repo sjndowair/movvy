3e9c75d921076f2eb7d2cb07a3358f4041546048
sjndowair <wodnjsdl1125@naver.com>
2024-07-19 21:27:00 +0900

    fix: TMDB API 코드 마이그레이션

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,26 @@ import { GlobalStyle } from "./components/Theme/global-style";
 import { useThemeMode } from "./contexts/themeCtx";
 import Home from "./pages/root";
 import Layout from "./components/layout";
-import { useEffect, useState } from "react";
-import { API_KEY } from "./constants/api.constant";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/thisIsTestPage",
+    element: <div>Wow This is real!s</div>,
+  },
+]);
 
 function App() {
   const { isDark } = useThemeMode();
-  const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    const getNowPlayingMovieList = async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
-        {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-          },
-        }
-      );
-
-      const result = await response.json();
-
-      // console.log(result);
-      setMovies(result);
-    };
-
-    getNowPlayingMovieList();
-  }, []);
-
-  useEffect(() => {
-    // console.log(movies);
-  }, [movies]);
 
   return (
     <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
       <GlobalStyle />
-      <Home />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
