e1b2e9abe809056d539c11c714b65cb382ef5af9
sjndowair <wodnjsdl1125@naver.com>
2024-07-19 21:21:46 +0900

    fix: 컴포넌트 분할
    header, mainCarousal, cardCarousal, notice, footer 컴포넌트 순으로 분할

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,31 +1,17 @@
-//내부 스테이트
 import { useEffect, useState } from "react";
-//캐러셀 임폴트용
-import CardCarousel from "../components/common/cardCarousel";
-import MainCarousel from "../components/pages/root/mainCarousel";
-//레이아웃 임폴트
-import Layout from "../components/layout";
+
 import NoticeContainer from "../components/common/noticeContainer";
-//더미데이터 가져오는거
-import { CAROUSEL_TITLE_LIST } from "../constants/nav-menus.constant";
-import { GNB_MENUS_LIST } from "../constants/nav-menus.constant";
-//스타일드 컴포넌트
-import {
-  MainTitleImg,
-  MainTitleName,
-  MoivesImgBox,
-  MoviesTitleName,
-  BackgroundDimEffectBox,
-  MainTitleOverView,
-  MainTitle,
-  SortBox,
-} from "./style";
+import Layout from "../components/layout";
+import MainCarouselComponent from "../components/pages/root/mainCarousel";
+import CardCarouselComponent from "../components/common/cardCarousel";
+
 import {
   HeaderToggleMenu,
   TabletGnb,
   TabletGnbList,
 } from "../components/pages/root/mainCarousel/style";
-import { CardDisChargeContainer } from "../components/Theme/global-style";
+
+import { CARD_MENU } from "../constants/card-menus.constant";
 import {
   getNowPlayingMovieList,
   getTopRatedMovieList,
@@ -36,26 +22,18 @@ import { Link } from "react-router-dom";
 import { getVideoByMovieId } from "../apis/videos.api";
 import { IVideo } from "../types/videos";
 import { getVideoPath } from "../utils/video.util";
+import { CardCollectionBox } from "./style";
 
 const Home = () => {
   const [headerToggle, setHeaderToggle] = useState<boolean>(false);
-  const [movies, setMovies] = useState<IMovie[]>();
-  const [rateMovies, setRateMovies] = useState<IMovie[]>();
-  const [tmpVideo, setTmpVideo] = useState<IVideo>();
   const [randomMovie, setRandomMovie] = useState<IMovie | null>(null);
 
   const handleToggleMenu = (): void => {
-    setHeaderToggle((prev) => (prev = !prev));
+    setHeaderToggle((prev) => !prev);
   };
 
   useEffect(() => {
-    getNowPlayingMovieList().then((res) => setMovies(res.results));
-  }, []);
-
-  useEffect(() => {
     getNowPlayingMovieList().then((res) => {
-      setMovies(res.results);
-      console.log(res);
       if (res.results.length > 0) {
         const randomMovieIndex = Math.floor(Math.random() * res.results.length);
         setRandomMovie(res.results[randomMovieIndex]);
@@ -63,94 +41,21 @@ const Home = () => {
     });
   }, []);
 
-  useEffect(() => {
-    getTopRatedMovieList().then((res) => {
-      setRateMovies(res.results);
-      // console.log(res);
-    });
-  });
-
-  useEffect(() => {
-    if (movies?.length) {
-      getVideoByMovieId(movies[0].id).then((res) =>
-        setTmpVideo(res.results[0])
-      );
-    }
-  }, [movies]);
-
   return (
-    <>
-      <Layout headerToggle={headerToggle} iconClickRotate={handleToggleMenu}>
-        <Link to={"/thisIsTestPage"}>
-          <button>클릭하면 임시페이지로 감 ㅋㅋ</button>
-        </Link>
-
-        {!tmpVideo ? (
-          <p>Now Loading...</p>
-        ) : (
-          <iframe
-            id="player"
-            width="640"
-            height="360"
-            src={getVideoPath(tmpVideo.key)}
-          />
-        )}
-        {randomMovie && (
-          <MainTitle>
-            <BackgroundDimEffectBox />
-            <MainTitleImg
-              src={getImagePath(randomMovie.backdrop_path)}
-            ></MainTitleImg>
-            <MainTitleName>{randomMovie.title}</MainTitleName>
-            <MainTitleOverView>{randomMovie.overview}</MainTitleOverView>
-          </MainTitle>
-        )}
-        <SortBox>
-          {!movies?.length ? (
-            <p>Now Loading...</p>
-          ) : (
-            movies.map((m, i) => (
-              <MoivesImgBox>
-                <div key={i}>
-                  <img
-                    width={"300px"}
-                    height={"auto"}
-                    src={getImagePath(m.backdrop_path)}
-                  />
-                  <MoviesTitleName
-                    style={{ color: "white", fontSize: "3.5rem" }}
-                  >
-                    {m.title}
-                  </MoviesTitleName>
-                </div>
-              </MoivesImgBox>
-            ))
-          )}
-        </SortBox>
-        <SortBox>
-          {!movies?.length ? (
-            <p>Now Loading...</p>
-          ) : (
-            rateMovies?.map((m, i) => (
-              <MoivesImgBox>
-                <div key={i}>
-                  <img
-                    width={"300px"}
-                    height={"auto"}
-                    src={getImagePath(m.backdrop_path)}
-                  />
-                  <MoviesTitleName
-                    style={{ color: "white", fontSize: "3.5rem" }}
-                  >
-                    {m.title}
-                  </MoviesTitleName>
-                </div>
-              </MoivesImgBox>
-            ))
-          )}
-        </SortBox>
-      </Layout>
-    </>
+    <Layout headerToggle={headerToggle} iconClickRotate={handleToggleMenu}>
+      <MainCarouselComponent />
+      <CardCollectionBox>
+        <CardCarouselComponent
+          title="Now Playing"
+          fetchMovies={getNowPlayingMovieList}
+        ></CardCarouselComponent>
+        <CardCarouselComponent
+          title="Top Rated"
+          fetchMovies={getTopRatedMovieList}
+        />
+      </CardCollectionBox>
+      <NoticeContainer></NoticeContainer>
+    </Layout>
   );
 };
 
--- src/pages/style.ts
+++ src/pages/style.ts
@@ -1,17 +1,15 @@
 import styled from "styled-components";
+import { mediaSize } from "../components/Theme/theme";
 
 export const SortBox = styled.div`
   display: flex;
-`;
-
-export const MoivesImgBox = styled.div`
-  width: 300px;
-  height: auto;
-  position: relative;
+  gap: 2rem;
+  justify-content: flex-start;
 `;
 
 export const MainTitle = styled.div`
   position: relative;
+  z-index: 2;
 `;
 export const MainTitleImg = styled.img`
   width: 100%;
@@ -35,22 +33,54 @@ export const MainTitleName = styled.h5`
   top: 30%;
   left: 4rem;
   font-size: 7rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  ${mediaSize.tablet} {
+    font-size: 5rem;
+  }
+  ${mediaSize.mobile} {
+    font-size: 3rem;
+  }
 `;
 export const MainTitleOverView = styled.p`
   position: absolute;
   color: rgb(220, 220, 220);
-  width: 90%;
+  width: 80%;
   z-index: 2;
   font-weight: 600;
   font-size: 1.75rem;
   left: 4rem;
   bottom: 20%;
   line-height: 1.8;
+  ${mediaSize.tablet} {
+    font-size: 1.25rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+  ${mediaSize.mobile} {
+    font: 0.75rem;
+    width: 80%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
 `;
 
-export const MoviesTitleName = styled.h5`
+export const TemporaryIframe = styled.iframe`
   position: absolute;
-  font-size: 2.5rem;
-  color: rgb(220, 220, 220);
-  top: 50%;
+  z-index: 0;
+`;
+
+export const CardCollectionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
