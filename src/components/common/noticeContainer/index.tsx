66f5d6b5fa0fc5d871e1721d5152608760bbc40a
sjndowair <wodnjsdl1125@naver.com>
2024-07-19 21:09:57 +0900

    fix: notice 컴포넌트 스타일 수정
    svg파일 삭제 및 스타일 수정으로 인한 컴포넌트 수정

--- src/components/common/noticeContainer/index.tsx
+++ src/components/common/noticeContainer/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useEffect, useState } from "react";
-
 import {
   Notice,
   NoticeContantsIndex,
@@ -9,54 +8,71 @@ import {
   NoticeArrowContainer,
   NoticeNextBtnContainer,
   NoticePrevBtnContainer,
+  SvgStyleSheetContainer,
 } from "./style";
 
 import { NOTICE_CONTENT_LIST } from "../../../constants/notice-content.constant";
 
 export const NoticeChevronUpIcon = (props: any) => {
   return (
-    <svg
-      dataSlot="icon"
-      fill="none"
-      strokeWidth={1.5}
-      stroke="gray"
+    <SvgStyleSheetContainer
+      fill="rgba(50, 50, 50, 0.5)"
+      strokeWidth={2}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
-      {...props}
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
         d="m4.5 15.75 7.5-7.5 7.5 7.5"
       />
-    </svg>
+    </SvgStyleSheetContainer>
   );
 };
 
 export const NoticeChevronDownIcon = (props: any) => {
   return (
-    <svg
-      dataSlot="icon"
-      fill="none"
-      strokeWidth={1.5}
-      stroke="gray"
+    <SvgStyleSheetContainer
+      fill="rgba(50, 50, 50, 0.5)"
+      strokeWidth={2}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
-      {...props}
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
         d="m19.5 8.25-7.5 7.5-7.5-7.5"
       />
-    </svg>
+    </SvgStyleSheetContainer>
   );
 };
 
 const NoticeContainer = () => {
   const [noticeIndex, setNoticeIndex] = useState<number>(0);
+  const [noticeState, setNoticeState] = useState({
+    isActive: 0,
+    isDown: false,
+  });
+
+  const clickPrevNoticeChangeEvent = () => {
+    setNoticeState((pre) => ({
+      isActive:
+        pre.isActive === 0 ? NOTICE_CONTENT_LIST.length - 1 : pre.isActive - 1,
+      isDown: false,
+    }));
+  };
+
+  const clickNextNoticeChangeEvent = () => {
+    setNoticeState((pre) => ({
+      isActive:
+        pre.isActive + 1 === NOTICE_CONTENT_LIST.length ? 0 : pre.isActive + 1,
+      isDown: true,
+    }));
+  };
+
+  console.log(noticeState.isActive);
 
   const prevNotice = () => {
     setNoticeIndex(
@@ -83,19 +99,19 @@ const NoticeContainer = () => {
           {NOTICE_CONTENT_LIST.map((item, index) => (
             <NoticeContantsIndex
               key={index}
-              style={{ display: index === noticeIndex ? "block" : "none" }}
+              isActive={index === noticeState.isActive}
+              isDown={noticeState.isDown}
             >
               {item}
             </NoticeContantsIndex>
           ))}
-          ;
         </ul>
       </WidthControlContainer>
       <NoticeArrowContainer>
-        <NoticeNextBtnContainer onClick={prevNotice}>
+        <NoticeNextBtnContainer onClick={clickPrevNoticeChangeEvent}>
           <NoticeChevronUpIcon />
         </NoticeNextBtnContainer>
-        <NoticePrevBtnContainer onClick={nextNotice}>
+        <NoticePrevBtnContainer onClick={clickNextNoticeChangeEvent}>
           <NoticeChevronDownIcon />
         </NoticePrevBtnContainer>
       </NoticeArrowContainer>
--- src/components/common/noticeContainer/style.ts
+++ src/components/common/noticeContainer/style.ts
@@ -1,14 +1,16 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
 export const Notice = styled.div`
-  background: #222;
+  width: 100%;
+  background: #111;
   color: #fff;
   height: 5rem;
   display: flex;
   align-items: center;
   border-top: solid 1px gray;
   border-bottom: solid 1px gray;
-  margin-top: 2rem;
+  margin-top: 3rem;
 `;
 
 export const WidthControlContainer = styled.div`
@@ -25,14 +27,80 @@ export const NoticeTitleContainer = styled.div`
   font-size: ${({ theme }) => theme.text.xxl};
 `;
 
-export const NoticeContantsIndex = styled.li`
+interface INoticeProps {
+  isActive: any;
+  isDown: boolean;
+}
+
+const slideInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const slideOutUp = keyframes`
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+`;
+
+const slideInDown = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const slideOutDown = keyframes`
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+`;
+
+export const NoticeContantsIndex = styled.li<INoticeProps>`
+  fill: rgba(50, 50, 50, 0.5);
   position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
+  transition: all 0.5s ease;
+  top: 42%;
+  cursor: pointer;
+
+  animation: ${({ isActive, isDown }) =>
+      isActive
+        ? isDown
+          ? slideInDown
+          : slideInUp
+        : isDown
+        ? slideOutDown
+        : slideOutUp}
+    0.5s forwards;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export const NoticeArrowContainer = styled.div`
-  margin-right: 1.25rem;
+  margin-right: 0.25rem;
+  width: 7rem;
 `;
 
 export const NoticePrevBtnContainer = styled.button`
@@ -43,4 +111,13 @@ export const NoticePrevBtnContainer = styled.button`
 export const NoticeNextBtnContainer = styled.button`
   width: 2.5rem;
   height: 2.5rem;
+`;
+
+export const SvgStyleSheetContainer = styled.svg`
+  box-shadow: rgba(60, 60, 60, 1) 0rem 0rem 0.5rem;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    fill: rgb(255, 111, 15);
+    box-shadow: rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;
+  }
 `;
