19e1e6eafe047a048e130eb9bd16e4ceeeb546cb
sjndowair <wodnjsdl1125@naver.com>
2024-07-27 00:04:23 +0900

    fix: 모달 오늘하루보지않기 버튼 활성화

--- src/components/layout/Modal/index.tsx
+++ src/components/layout/Modal/index.tsx
@@ -4,35 +4,68 @@ import {
   ModalBackgroundDeemContainer,
   ModalInnerContainer,
   Span,
+  Div,
+  ModalBTN,
+  CheckBoxContainer,
 } from "./style";
 import { Xcircle } from "../../Context/\bindex";
 
+const TIME_STORAGE = new Date().getDate().toString();
+
 const ModalComponet = () => {
   const [modalCondition, setModalCondition] = useState<boolean>(true);
+  const [checkToday, setCheckToday] = useState<boolean>(false);
+
+  const checkOppositionEvent = () => {
+    setCheckToday(!checkToday);
+  };
 
   const closeModal = (): void => {
     setModalCondition(false);
   };
 
+  const todayCloseModal = (): void => {
+    if (checkToday) {
+      localStorage.setItem("otherTime", TIME_STORAGE);
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    const GET_NOW_TIME = localStorage.getItem("otherTime");
+    if (GET_NOW_TIME === TIME_STORAGE) {
+      setModalCondition(false);
+    }
+  }, []);
+
   useEffect(() => {
     if (modalCondition) {
       document.body.style.overflowY = "hidden";
     } else {
-      document.body.style.overflowY = "initial";
+      document.body.style.overflowY = "unset";
     }
     return () => {
       document.body.style.overflowY = "unset";
     };
-  }, [closeModal]);
+  }, [modalCondition]);
 
   return (
     <>
       {modalCondition && (
         <ModalBackgroundDeemContainer>
           <ModalInnerContainer>
-            <div onClick={closeModal}>
-              <Xcircle></Xcircle>
-            </div>
+            <Div onClick={closeModal}>
+              <Xcircle />
+            </Div>
+            <CheckBoxContainer>
+              <input
+                type="checkBox"
+                onChange={checkOppositionEvent}
+                checked={checkToday}
+              />
+
+              <ModalBTN onClick={todayCloseModal}>오늘하루보지않기</ModalBTN>
+            </CheckBoxContainer>
             <H5>
               <Span>Movvy가?</Span>
               <span>오늘하루 무료라고!?</span>
--- src/components/layout/Modal/style.ts
+++ src/components/layout/Modal/style.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { MODAL_URL } from "../../../utils/modal.utill";
+import { mediaSize } from "../../Theme/theme";
 
 export const ModalBackgroundDeemContainer = styled.div`
   width: 100%;
@@ -14,14 +15,17 @@ export const ModalBackgroundDeemContainer = styled.div`
 
 export const ModalInnerContainer = styled.div`
   background: url(${MODAL_URL}) center/cover no-repeat;
-  width: 70%;
-  height: 50vw;
-  font-size: 5.5rem;
+  width: 50%;
+  height: 40vw;
   font-weight: ${({ theme }) => theme.font.bold};
   background-color: rgba(255, 255, 255, 1);
   position: absolute;
   border-radius: 1rem;
   box-shadow: 0px 4px 9px 9px rgba(255, 111, 15, 0.5);
+  ${mediaSize.custom(700)} {
+    font-size: 2.5rem;
+    height: 50vw;
+  }
 `;
 export const H5 = styled.h5`
   position: absolute;
@@ -33,10 +37,38 @@ export const H5 = styled.h5`
   color: rgba(0, 0, 0, 1);
   left: 50%;
   width: 100%;
+  font-size: 5.5rem;
   text-shadow: 2px 2px rgba(255, 111, 15, 0.5);
   transform: translateX(-50%);
+  ${mediaSize.custom(1000)} {
+    font-size: 3.5rem;
+  }
+  ${mediaSize.custom(700)} {
+    font-size: 2.5rem;
+  }
 `;
 
 export const Span = styled.span`
   font-size: 7rem;
+  ${mediaSize.custom(700)} {
+    font-size: 3.5rem;
+  }
+`;
+
+export const Div = styled.div`
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const CheckBoxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  gap: 0.5rem;
+  bottom: -2rem;
+  right: 0;
+`;
+
+export const ModalBTN = styled.button`
+  color: rgba(255, 255, 255, 1);
 `;
