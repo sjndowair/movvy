dc62fd9afe64828487923892d270f1262d8d43f9
sjndowair <wodnjsdl1125@naver.com>
2024-08-02 23:26:29 +0900

    feat: tv serise  타입 정의 추가

--- src/types/movieList.d.ts
+++ src/types/movieList.d.ts
@@ -25,3 +25,26 @@ export interface IMovieListResponse {
   total_pages: number;
   total_results: number;
 }
+
+interface ITvSerise {
+  backdrop_path: string | null;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface ITvSeriseResponse {
+  page: number;
+  results: SearchResult[];
+  total_pages: number;
+  total_results: number;
+}
