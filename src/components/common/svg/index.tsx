ea18396e85a6eab68e8b341cb3e665094e05b4f9
sjndowair <wodnjsdl1125@naver.com>
2024-08-05 21:49:02 +0900

    fix: svg 컴포넌트 수정
    styled 컴포넌트 삭제

--- src/components/common/svg/index.tsx
+++ src/components/common/svg/index.tsx
@@ -1,99 +1,184 @@
-import { ScrollTopSvg, MagnifySvg, ArrowSvg, XcircleSvg } from "./style";
+import { ScrollTopSvg } from "./style";
 import { svgProperty } from "../../../constants/svg.constant";
+import { useState } from "react";
 
-export const RightArrowButton = () => {
-  const { width, height, fill } = svgProperty;
+interface ISvgProps {
+  fill?: string;
+  size?: string;
+  xmlns?: string;
+  stroke?: string;
+  strokeWidth?: string;
+  viewBox?: string;
+  boxShadow?: string;
+}
+
+export const RightArrowButton = ({
+  fill = "rgba(50, 50, 50, 0.8)",
+  size = "30",
+  xmlns = "http://www.w3.org/2000/svg",
+  viewBox = "0 0 24 24",
+  stroke = "none",
+  strokeWidth = "1",
+}: ISvgProps) => {
+  const [hover, setHover] = useState<boolean>(false);
   return (
-    <ArrowSvg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill={fill}
-      width={width}
-      height={height}
+    <svg
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      xmlns={xmlns}
+      viewBox={viewBox}
+      fill={hover ? "rgb(255, 111, 15)" : fill}
+      width={size}
+      height={size}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      style={{
+        transition: "all 0.5s ease",
+        boxShadow: hover ? "rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;" : fill,
+      }}
     >
       <path
         fillRule="evenodd"
         d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
         clipRule="evenodd"
       />
-    </ArrowSvg>
+    </svg>
   );
 };
 
-export const LeftArrowButton = () => {
-  const { width, height, fill } = svgProperty;
+export const LeftArrowButton = ({
+  fill = "rgba(50, 50, 50, 0.8)",
+  size = "30",
+  xmlns = "http://www.w3.org/2000/svg",
+  viewBox = "0 0 24 24",
+  stroke = "none",
+  strokeWidth = "1",
+}: ISvgProps) => {
+  const [hover, setHover] = useState<boolean>(false);
   return (
-    <ArrowSvg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill={fill}
-      width={width}
-      height={height}
+    <svg
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      xmlns={xmlns}
+      viewBox={viewBox}
+      fill={hover ? "rgb(255, 111, 15)" : fill}
+      width={size}
+      height={size}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      style={{
+        transition: "all 0.5s ease",
+        boxShadow: hover ? "rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;" : fill,
+      }}
     >
       <path
         fillRule="evenodd"
         d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z"
         clipRule="evenodd"
       />
-    </ArrowSvg>
+    </svg>
   );
 };
 
-export const UpArrowButton = () => {
-  const { width, height, fill } = svgProperty;
+export const UpArrowButton = ({
+  fill = "rgba(50, 50, 50, 0.8)",
+  size = "30",
+  xmlns = "http://www.w3.org/2000/svg",
+  viewBox = "0 0 24 24",
+  stroke = "none",
+  strokeWidth = "1",
+}: ISvgProps) => {
+  const [hover, setHover] = useState<boolean>(false);
   return (
-    <ArrowSvg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill={fill}
-      width={width}
-      height={height}
+    <svg
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      xmlns={xmlns}
+      viewBox={viewBox}
+      fill={hover ? "rgb(255, 111, 15)" : fill}
+      width={size}
+      height={size}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      style={{
+        transition: "all 0.5s ease",
+        boxShadow: hover ? "rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;" : fill,
+      }}
     >
       <path
         fillRule="evenodd"
         d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z"
         clipRule="evenodd"
       />
-    </ArrowSvg>
+    </svg>
   );
 };
 
-export const DownArrowButton = () => {
-  const { width, height, fill } = svgProperty;
+export const DownArrowButton = ({
+  fill = "rgba(50, 50, 50, 0.8)",
+  size = "30",
+  xmlns = "http://www.w3.org/2000/svg",
+  viewBox = "0 0 24 24",
+  stroke = "none",
+  strokeWidth = "1",
+}: ISvgProps) => {
+  const [hover, setHover] = useState<boolean>(false);
   return (
-    <ArrowSvg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill={fill}
-      width={width}
-      height={height}
-      className="size-6"
+    <svg
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      xmlns={xmlns}
+      viewBox={viewBox}
+      fill={hover ? "rgb(255, 111, 15)" : fill}
+      width={size}
+      height={size}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      style={{
+        transition: "all 0.5s ease",
+        boxShadow: hover ? "rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;" : fill,
+      }}
     >
       <path
         fillRule="evenodd"
         d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
         clipRule="evenodd"
       />
-    </ArrowSvg>
+    </svg>
   );
 };
 
-export const Magnifiy = () => {
-  const { fill, width, height } = svgProperty;
+export const Magnifiy = ({
+  fill = "rgba(50, 50, 50, 0.8)",
+  size = "24",
+  xmlns = "http://www.w3.org/2000/svg",
+  viewBox = "0 0 24 24",
+  stroke = "none",
+  strokeWidth = "1",
+}: ISvgProps) => {
+  const [hover, setHover] = useState<boolean>(false);
   return (
-    <MagnifySvg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill={fill}
-      width={width}
-      height={height}
+    <svg
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      xmlns={xmlns}
+      viewBox={viewBox}
+      fill={hover ? "rgb(255, 111, 15)" : fill}
+      width={size}
+      height={size}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      style={{
+        transition: "all 0.5s ease",
+        boxShadow: hover ? "rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;" : fill,
+      }}
     >
       <path
         fillRule="evenodd"
         d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
         clipRule="evenodd"
       />
-    </MagnifySvg>
+    </svg>
   );
 };
 
@@ -118,21 +203,38 @@ export const ArrowUpCircle = () => {
   );
 };
 
-export const Xcircle = () => {
-  const { width, fill, height } = svgProperty;
+export const Xcircle = ({
+  fill = "rgba(255, 255, 255, 0.8)",
+  size = "24",
+  xmlns = "http://www.w3.org/2000/svg",
+  viewBox = "0 0 24 24",
+  stroke = "none",
+  strokeWidth = "1",
+}: ISvgProps) => {
+  const [hover, setHover] = useState<boolean>(false);
   return (
-    <XcircleSvg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill={fill}
-      width={width}
-      height={height}
+    <svg
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      xmlns={xmlns}
+      viewBox={viewBox}
+      fill={hover ? "rgb(255, 111, 15)" : fill}
+      width={size}
+      height={size}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      style={{
+        cursor: "pointer",
+        float: "right",
+        transition: "all 0.5s ease",
+        boxShadow: hover ? "rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;" : fill,
+      }}
     >
       <path
         fillRule="evenodd"
         d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
         clipRule="evenodd"
       />
-    </XcircleSvg>
+    </svg>
   );
 };
--- src/components/common/svg/style.ts
+++ src/components/common/svg/style.ts
@@ -1,29 +1,6 @@
 import styled from "styled-components";
 import { ISvgProperty } from "../../../types/svg";
 
-export const MagnifySvg = styled.svg<ISvgProperty>`
-  fill: ${({ fill }) => fill.white};
-  width: ${({ width }) => width.xl};
-  height: ${({ height }) => height.xl};
-  cursor: pointer;
-  transition: all 0.5s ease;
-  &:hover {
-    fill: ${({ fill }) => fill.primary};
-  }
-`;
-
-export const ArrowSvg = styled.svg<ISvgProperty>`
-  fill: rgba(50, 50, 50, 0.5);
-  width: ${({ width }) => width.xxl};
-  height: ${({ height }) => height.xxl};
-  box-shadow: rgba(60, 60, 60, 1) 0rem 0rem 0.5rem;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    fill: ${({ fill }) => fill.primary};
-    box-shadow: rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;
-  }
-`;
-
 export const ScrollTopSvg = styled.svg<ISvgProperty>`
   fill: rgba(0, 0, 0, 1);
   stroke: ${({ theme }) => theme.color.primary};
@@ -34,17 +11,5 @@ export const ScrollTopSvg = styled.svg<ISvgProperty>`
   &:hover {
     fill: ${({ fill }) => fill.white};
     box-shadow: rgba(255, 111, 15, 0.5) 0rem 0rem 1rem;
-  }
-`;
-
-export const XcircleSvg = styled.svg<ISvgProperty>`
-  fill: rgba(0, 0, 0, 1);
-  float: right;
-  cursor: pointer;
-  width: ${({ width }) => width.xxl};
-  height: ${({ height }) => height.xxl};
-  transition: all 0.2s ease;
-  &:hover {
-    fill: ${({ theme }) => theme.color.background};
   }
 `;
