2781e51cded4a2015c068c47808007d75dd9b5d4
sjndowair <wodnjsdl1125@naver.com>
2024-08-10 22:40:04 +0900

    feat: tv series api 추가

--- src/apis/seriesList.api.ts
+++ src/apis/seriesList.api.ts
@@ -5,12 +5,43 @@ import {
 } from "../constants/api.constant";
 import { ITvSeriseResponse } from "../types/movieList";
 
-export const getTvSeriesList = async (
+export const getAiringTodaySeriesList = async (
   reqInit: RequestInit = REQUEST_INIT_OBJECT
 ): Promise<ITvSeriseResponse> => {
   return await (
     await fetch(
-      `${BASE_URL}/tv/airing_today?api_key=${API_KEY}language=ko-KR&page=1&region=kr`,
+      `${BASE_URL}/tv/airing_today?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`,
+      reqInit
+    )
+  ).json();
+};
+export const getOnTheAirSeriesList = async (
+  reqInit: RequestInit = REQUEST_INIT_OBJECT
+): Promise<ITvSeriseResponse> => {
+  return await (
+    await fetch(
+      `${BASE_URL}/tv/on_the_air?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`,
+      reqInit
+    )
+  ).json();
+};
+export const getPopularSeriesList = async (
+  reqInit: RequestInit = REQUEST_INIT_OBJECT
+): Promise<ITvSeriseResponse> => {
+  return await (
+    await fetch(
+      `${BASE_URL}/tv/popular?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`,
+      reqInit
+    )
+  ).json();
+};
+
+export const getTopRatedSeriesList = async (
+  reqInit: RequestInit = REQUEST_INIT_OBJECT
+): Promise<ITvSeriseResponse> => {
+  return await (
+    await fetch(
+      `${BASE_URL}/tv/top_rated?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`,
       reqInit
     )
   ).json();
