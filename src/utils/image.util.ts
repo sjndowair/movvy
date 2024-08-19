b66294d9f0663ad26c5c9d255dbc4ea9b939f84f
sjndowair <wodnjsdl1125@naver.com>
2024-08-20 00:57:08 +0900

    fix: imagePath 옵셔널체이닝 추가

--- src/utils/image.util.ts
+++ src/utils/image.util.ts
@@ -1,5 +1,8 @@
 const BASE_URL = "https://image.tmdb.org/t/p";
 
-export const getImagePath = (id: string, format: string = "original") => {
+export const getImagePath = (
+  id?: string | null,
+  format: string = "original"
+) => {
   return `${BASE_URL}/${format}/${id}`;
 };
