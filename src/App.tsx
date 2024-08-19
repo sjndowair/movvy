cf707d99a3b682c781369b2af191e71aea1e7e2f
sjndowair <wodnjsdl1125@naver.com>
2024-08-20 00:56:34 +0900

    fix: hashRouter 컴포넌트 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { ThemeProvider } from "styled-components";
-import { Route, Routes } from "react-router-dom";
 import { darkTheme, lightTheme } from "./components/Theme/theme";
 import { GlobalStyle } from "./components/Theme/global-style";
 import { useThemeMode } from "./contexts/themeCtx";
 import Home from "./pages/root";
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
 import SearchPage from "./pages/Searchpage";
+import Series from "./pages/Series";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import VideoPage from "./pages/VideoPage";
 
 function App() {
   const { isDark } = useThemeMode();
@@ -14,9 +14,13 @@ function App() {
   return (
     <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
       <GlobalStyle />
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ThisIsSearchPage" element={<SearchPage />} />
+        <Route path={"/"} element={<Home />}>
+          <Route path={"/movie/:movieId"} element={<Home />} />
+        </Route>
+        <Route path={"/search"} element={<SearchPage />} />
+        <Route path={"/series"} element={<Series />} />
       </Routes>
     </ThemeProvider>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
