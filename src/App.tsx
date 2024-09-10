81da1890ef55066ae62b9f5bf885a2b150d5b06b
sjndowair <wodnjsdl1125@naver.com>
2024-09-10 22:22:31 +0900

    fix: router 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { darkTheme, lightTheme } from "./components/Theme/theme";
 import { GlobalStyle } from "./components/Theme/global-style";
 import { useThemeMode } from "./contexts/themeCtx";
 import Home from "./pages/root";
-import SearchPage from "./pages/Searchpage";
+import SearchPage from "./pages/search/Search";
 import Series from "./pages/Series";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import VideoPage from "./pages/VideoPage";
+import VideoPage from "./components/layout/video/Video";
+import Detail from "./pages/detail/Detail";
 
 function App() {
   const { isDark } = useThemeMode();
@@ -19,8 +20,11 @@ function App() {
         <Route path={"/"} element={<Home />}>
           <Route path={"/movie/:movieId"} element={<Home />} />
         </Route>
-        <Route path={"/search"} element={<SearchPage />} />
-        <Route path={"/series"} element={<Series />} />
+        <Route path={"/search"} element={<SearchPage />}></Route>
+        <Route path={"/search/:type/:id"} element={<Detail />} />
+        <Route path={"/series"} element={<Series />}>
+          <Route path={"/series/:seriesId"} element={<Series />} />
+        </Route>
       </Routes>
     </ThemeProvider>
   );
