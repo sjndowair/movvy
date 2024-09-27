b53fe2f2bb4612c44ce59855ae148001f5b22770
sjndowair <wodnjsdl1125@naver.com>
2024-09-27 12:20:53 +0900

    fix: router 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./components/Theme/theme";
 import { GlobalStyle } from "./components/Theme/global-style";
 import { useThemeMode } from "./contexts/themeCtx";
-import Home from "./pages/root";
+import Home from "./pages/root/root";
 import SearchPage from "./pages/search/Search";
 import Series from "./pages/Series";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import VideoPage from "./components/layout/video/Video";
 import Detail from "./pages/detail/Detail";
 
 function App() {
@@ -15,17 +14,16 @@ function App() {
   return (
     <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
       <GlobalStyle />
-
-      <Routes>
-        <Route path={"/"} element={<Home />}>
-          <Route path={"/movie/:movieId"} element={<Home />} />
-        </Route>
-        <Route path={"/search"} element={<SearchPage />}></Route>
-        <Route path={"/search/:type/:id"} element={<Detail />} />
-        <Route path={"/series"} element={<Series />}>
-          <Route path={"/series/:seriesId"} element={<Series />} />
-        </Route>
-      </Routes>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:movieId" element={<Home />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="search/:type/:id" element={<Detail />} />
+          <Route path="series" element={<Series />} />
+          <Route path="series/:seriesId" element={<Series />} />
+        </Routes>
+      </HashRouter>
     </ThemeProvider>
   );
 }
