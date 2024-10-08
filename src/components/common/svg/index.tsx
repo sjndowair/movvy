21942ab0dbee0754370c9fd9e82ceb479dc71117
sjndowair <wodnjsdl1125@naver.com>
2024-10-09 00:44:59 +0900

    delete: svg styled 컴포넌트 삭제
    styled 텀포넌트 사용 시 유지보수에 용이하지않아 svg interface로 속성을 주어 직접 하나의 컴포넌트 안에서 수정할수있는게 용이 하다 판단 하여 수정

--- src/components/common/svg/index.tsx
+++ src/components/common/svg/index.tsx
@@ -1,17 +1,6 @@
-import { ScrollTopSvg } from "./style";
-import { svgProperty } from "../../../constants/svg.constant";
 import { useState } from "react";
-import { fill } from "lodash";
 
-interface ISvgProps {
-  fill?: string;
-  size?: string;
-  xmlns?: string;
-  stroke?: string;
-  strokeWidth?: string;
-  viewBox?: string;
-  boxShadow?: string;
-}
+import { ISvgProps } from "../../../types/svg";
 
 export const Back = () => {
   const [hover, setHover] = useState<boolean>(false);
@@ -211,16 +200,18 @@ export const Magnifiy = ({
   );
 };
 
-export const ArrowUpCircle = () => {
-  const { fill, width, height } = svgProperty;
+export const ArrowUpCircle = ({
+  fill = "rgba(0, 0, 0, 0)",
+  size = "50",
+}: ISvgProps) => {
   return (
-    <ScrollTopSvg
+    <svg
       xmlns="http://www.w3.org/2000/svg"
       fill={fill}
-      width={width}
-      height={height}
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
+      strokeWidth={1}
       stroke="currentColor"
     >
       <path
@@ -228,7 +219,7 @@ export const ArrowUpCircle = () => {
         strokeLinejoin="round"
         d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
       />
-    </ScrollTopSvg>
+    </svg>
   );
 };
 
@@ -268,7 +259,7 @@ export const Xcircle = ({
   );
 };
 
-export const StarRate = ({ voteAverage, size = 24 }: any) => {
+export const StarRate = ({ voteAverage, size = "24" }: any) => {
   const totalStar = () => 5;
   const totalRate = () => {
     return voteAverage / 2;
@@ -327,5 +318,47 @@ export const StarRate = ({ voteAverage, size = 24 }: any) => {
         );
       })}
     </div>
+  );
+};
+
+export const IsThemeBright = ({ size = "30" }: ISvgProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="rgb(255, 111, 15)"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="rgba(0,0,0,1)"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+      />
+    </svg>
+  );
+};
+
+export const IsThemeDark = ({ size = "30" }: ISvgProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="rgb(255, 111, 15)"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="rgba(0,0,0,1)"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+      />
+    </svg>
   );
 };
