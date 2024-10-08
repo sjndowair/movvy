5716e766a45eac221f33f957befca5f26aa236cf
sjndowair <wodnjsdl1125@naver.com>
2024-10-09 00:43:33 +0900

    fix: cardCarousal 수정
    스타일 비율 수정 및 로직 수정 useCallback 사용 시  window innerwidth 가 변경되지않으면 캐러셀 동작 하지않아 수정

--- src/components/common/cardCarousel/index.tsx
+++ src/components/common/cardCarousel/index.tsx
@@ -1,165 +1,145 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getImagePath } from "../../../utils/image.util";
-import { IMovie } from "../../../types/movieList";
-import { ITvSerise } from "../../../types/movieList";
 import {
-    NextBtn,
-    Slide,
-    SlideContainer,
-    PrevBtn,
-    MoviesImgBox,
-    CardImg,
-    MoviesTitleName,
-    TitleEncaseContainer,
-    CardTitle,
-    ArrowButtonContainer,
+  NextBtn,
+  Slide,
+  SlideContainer,
+  PrevBtn,
+  MoviesImgBox,
+  CardImg,
+  MoviesTitleName,
+  TitleEncaseContainer,
+  CardTitle,
+  ArrowButtonContainer,
 } from "./style";
 import { RightArrowButton, LeftArrowButton } from "../svg/index";
 import { useNavigate } from "react-router-dom";
-
-type TApiType = "movie" | "series"; // types 디렉터리에 분리
-
-// ICardCarouselComponentProps
-interface ISlideProps {
-    IMovie?: IMovie[]; // 인터페이스명과 프롭스명의 규칙
-    ITvSerise?: ITvSerise[];
-    ApiType?: TApiType; // 카멜케이스
-    title?: string;
-    hiddenCard: number[];
-    setHiddenCard: React.Dispatch<React.SetStateAction<number[]>>;
-}
+import {
+  ICardCarousalProps,
+  IOnClickProprs,
+} from "../../../types/cardCarousalList";
+import { useThemeMode } from "../../../contexts/themeCtx";
 
 const CardCarouselComponent = ({
-    IMovie,
-    ITvSerise,
-    title,
-    hiddenCard,
-    setHiddenCard,
-}: ISlideProps) => {
-    const [move, setMove] = useState<boolean>(false);
-    const [index, setIndex] = useState<number>(0);
-    const [carousalEvent, setCarousalEvent] = useState<boolean>(true);
-    const [reaction, setReaction] = useState<number>();
+  IMovie,
+  ITvSerise,
+  title,
+  hiddenCard,
+  setHiddenCard,
+}: ICardCarousalProps) => {
+  const [index, setIndex] = useState<number>(0);
+  const [carousalEvent, setCarousalEvent] = useState<boolean>(true);
 
-    const navigate = useNavigate();
+  const { isDark } = useThemeMode();
+  const navigate = useNavigate();
 
-    const onMovieClick = (movieId: string) => {
-        navigate(`/movie/${movieId}`);
-        setHiddenCard((pre) => [...pre, Number(movieId)]);
+  const isCarousalIndex = useCallback(() => {
+    if (IMovie) return IMovie.length - 1;
+    if (ITvSerise) return ITvSerise.length - 1;
+    return 0;
+  }, [IMovie, ITvSerise]);
+
+  const isCarousalResizeNumber = () => {
+    if (window?.innerWidth >= 1500) {
+      return 7;
+    } else if (window?.innerWidth >= 1300) {
+      return 6;
+    } else if (window?.innerWidth >= 1080) {
+      return 5;
+    } else if (window?.innerWidth >= 800) {
+      return 4;
+    }
+    return 3;
+  };
+  const onMoveCarousal = () => {
+    setCarousalEvent(true);
+  };
+
+  const onNextMove = () => {
+    if (index < isCarousalIndex() - isCarousalResizeNumber() + 1) {
+      onMoveCarousal();
+      setIndex((pre) => pre + isCarousalResizeNumber());
+    }
+  };
+
+  const onPrevMove = () => {
+    if (index > 0) {
+      onMoveCarousal();
+      setIndex((pre) => pre - isCarousalResizeNumber());
+    }
+  };
+
+  useEffect(() => {
+    const isWindowResize = () => {
+      setIndex(isCarousalResizeNumber()!);
     };
 
-    const onSeriesClick = (seriesId: string) => {
-        navigate(`/series/${seriesId}`);
-        setHiddenCard((pre) => [...pre, Number(seriesId)]);
-    };
+    window?.addEventListener("resize", isWindowResize);
 
-    const programIndex = () => {
-        if (IMovie && IMovie.length) {
-            return IMovie.length - 1;
-        } else if (ITvSerise && ITvSerise.length) {
-            return ITvSerise.length - 1;
-        } else {
-            return 0;
-        }
-    };
-    const moveState = () => setMove((pre) => !pre);
+    return () => window?.removeEventListener("resize", isWindowResize);
+  }, [isCarousalResizeNumber()]);
 
-    const inMoveCollectFunction = () => {
-        setCarousalEvent(true);
-        moveState();
-    };
+  const onClickItem = ({ seriesId, movieId }: IOnClickProprs) => {
+    if (movieId) {
+      navigate(`/movie/${movieId}`);
+      setHiddenCard((pre) => [...pre, Number(movieId)]);
+      return;
+    }
+    if (seriesId) {
+      navigate(`/series/${seriesId}`);
+      setHiddenCard((pre) => [...pre, Number(seriesId)]);
+      return;
+    }
+  };
 
-    const screenReactionType = () => {
-        if (window?.innerWidth >= 1500) {
-            return 7;
-        } else if (window?.innerWidth >= 1300) {
-            return 6;
-        } else if (window?.innerWidth >= 1080) {
-            return 5;
-        } else if (window?.innerWidth >= 800) {
-            return 4;
-        }
-        return 3;
-    };
-
-    useEffect(() => {
-        const carousalResize = () => {
-            setIndex(screenReactionType()!);
-        };
-
-        window.addEventListener("resize", carousalResize);
-
-        return () => window.removeEventListener("resize", carousalResize);
-    }, [screenReactionType()]);
-
-    const nextMove = () => {
-        if (index < programIndex() - screenReactionType()! + 1) {
-            inMoveCollectFunction();
-            setIndex((pre) => pre + screenReactionType()!);
-        }
-    };
-
-    const prevMove = () => {
-        if (index > 0) {
-            inMoveCollectFunction();
-            setIndex((pre) => pre - screenReactionType()!);
-        }
-    };
-
-    return (
-        <>
-            <TitleEncaseContainer>
-                <CardTitle>{title}</CardTitle>
-                <ArrowButtonContainer>
-                    <PrevBtn onClick={prevMove}>
-                        <LeftArrowButton />
-                    </PrevBtn>
-                    <NextBtn onClick={nextMove}>
-                        <RightArrowButton />
-                    </NextBtn>
-                </ArrowButtonContainer>
-                <SlideContainer>
-                    {IMovie &&
-                        IMovie?.map((m) => (
-                            <Slide
-                                key={m.id}
-                                $hiddenCard={hiddenCard.includes(m.id)}
-                                $index={index}
-                                onClick={() => onMovieClick(m.id.toString())}
-                                $cards={IMovie?.length}
-                                $reaction={screenReactionType()!}
-                            >
-                                <MoviesImgBox>
-                                    <CardImg
-                                        src={getImagePath(m.poster_path)}
-                                        alt={m.overview}
-                                    />
-                                    <MoviesTitleName>{m.title}</MoviesTitleName>
-                                </MoviesImgBox>
-                            </Slide>
-                        ))}
-                    {ITvSerise &&
-                        ITvSerise?.map((m) => (
-                            <Slide
-                                $hiddenCard={hiddenCard.includes(m.id)}
-                                key={m.id}
-                                $index={index}
-                                $cards={ITvSerise?.length}
-                                onClick={() => onSeriesClick(m.id.toString())}
-                                $reaction={screenReactionType()!}
-                            >
-                                <MoviesImgBox>
-                                    <CardImg
-                                        src={getImagePath(m.poster_path)}
-                                        alt={m.overview}
-                                    />
-                                    <MoviesTitleName>{m.name}</MoviesTitleName>
-                                </MoviesImgBox>
-                            </Slide>
-                        ))}
-                </SlideContainer>
-            </TitleEncaseContainer>
-        </>
-    );
+  return (
+    <>
+      <TitleEncaseContainer>
+        <CardTitle>{title}</CardTitle>
+        <ArrowButtonContainer>
+          <PrevBtn onClick={onPrevMove}>
+            <LeftArrowButton />
+          </PrevBtn>
+          <NextBtn onClick={onNextMove}>
+            <RightArrowButton />
+          </NextBtn>
+        </ArrowButtonContainer>
+        <SlideContainer>
+          {IMovie &&
+            IMovie?.map((m) => (
+              <Slide
+                key={m.id}
+                $hiddenCard={hiddenCard.includes(m.id)}
+                $index={index}
+                onClick={() => onClickItem({ movieId: m?.id.toString() })}
+                $cards={IMovie?.length}
+                $reaction={isCarousalResizeNumber()!}
+              >
+                <MoviesImgBox $isDark={isDark}>
+                  <CardImg src={getImagePath(m.poster_path)} alt={m.overview} />
+                  <MoviesTitleName>{m.title}</MoviesTitleName>
+                </MoviesImgBox>
+              </Slide>
+            ))}
+          {ITvSerise &&
+            ITvSerise?.map((m) => (
+              <Slide
+                $hiddenCard={hiddenCard.includes(m.id)}
+                key={m.id}
+                $index={index}
+                $cards={ITvSerise?.length}
+                onClick={() => onClickItem({ seriesId: m?.id.toString() })}
+                $reaction={isCarousalResizeNumber()!}
+              >
+                <MoviesImgBox $isDark={isDark}>
+                  <CardImg src={getImagePath(m.poster_path)} alt={m.overview} />
+                  <MoviesTitleName>{m.name}</MoviesTitleName>
+                </MoviesImgBox>
+              </Slide>
+            ))}
+        </SlideContainer>
+      </TitleEncaseContainer>
+    </>
+  );
 };
 export default CardCarouselComponent;
--- src/components/common/cardCarousel/style.ts
+++ src/components/common/cardCarousel/style.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
-import { mediaQuery } from "../../Theme/theme";
+import { mediaSize } from "../../theme/theme";
+import {
+  IMoviesImgBoxProps,
+  ISlideProps,
+} from "../../../types/cardCarousalList";
 
 export const ArrowButtonContainer = styled.div`
   position: relative;
@@ -24,42 +28,36 @@ export const SlideContainer = styled.div`
   display: flex;
   position: relative;
   width: 100%;
+  height: auto;
+  padding-bottom: 5rem;
   overflow: hidden;
-  background-color: rgba(0, 0, 0, 1);
 `;
-
-interface ISlideProps {
-  $index: number;
-  $cards: number;
-  $reaction: number;
-  $hiddenCard: boolean;
-}
 
 export const Slide = styled.div<ISlideProps>`
   display: flex;
   opacity: ${(props) => (props.$hiddenCard ? 0 : 1)};
   position: relative;
-  height: 55vh;
   justify-content: center;
   transition: all 0.4s ease;
   transform: translateX(-${(props) => props.$index * 100}%);
-  min-width: calc(100% / ${(props) => props.$reaction});
-  gap: 1.5rem;
-
-  ${mediaQuery(500)} {
-    height: 50vh;
+  flex: 0 0 calc(100% / ${(props) => props.$reaction});
+  width: 100%;
+  padding: 0 2rem;
+  ${mediaSize.tablet} {
+    padding: 0 1rem;
   }
 `;
 
-export const MoviesImgBox = styled.div`
+export const MoviesImgBox = styled.div<IMoviesImgBoxProps>`
   position: relative;
   height: 100%;
   z-index: 1;
   cursor: pointer;
-  color: rgba(255, 255, 255, 1);
+  color: ${({ theme }) => theme.color.average};
   text-align: center;
-  padding-top: 3rem;
   transition: all 0.5s ease;
+  box-shadow: 0, 10px 10px rgba(0, 0, 0, 1), 0 10px 10px rgba(0, 0, 0, 0, 0.6);
+
   &:hover {
     transform: scale(1.2);
     z-index: 10;
@@ -72,23 +70,10 @@ export const MoviesImgBox = styled.div`
 `;
 
 export const CardImg = styled.img`
-  width: 200px;
-  max-height: 300px;
-  min-height: 300px;
-  height: 100%;
-
-  ${mediaQuery(800)} {
-    width: 200px;
-  }
-  ${mediaQuery(650)} {
-    width: 180px;
-  }
-  ${mediaQuery(450)} {
-    width: 140px;
-    height: 100%;
-    max-height: 250px;
-    min-height: 250pxㄴ;
-  }
+  width: 100%;
+  max-width: 220px;
+  aspect-ratio: 12/18;
+  height: auto;
 `;
 
 export const MoviesTitleName = styled.h5`
@@ -111,7 +96,6 @@ export const CardTitle = styled.h2`
   margin-left: 2.75rem;
   margin-bottom: 2.75rem;
   font-weight: 700;
-  text-transform: uppercase;
   font-style: italic;
   color: ${({ theme }) => theme.color.primary};
 `;
