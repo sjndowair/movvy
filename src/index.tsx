c163b6c3eb0ba8348fc470d9e74887e60ea7679e
sjndowair <wodnjsdl1125@naver.com>
2024-10-09 00:42:07 +0900

    delete: 사용하지않는 devtool 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,21 @@ import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { ThemeModeProvider } from "./contexts/themeCtx";
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <QueryClientProvider client={queryClient}>
-        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-        <React.StrictMode>
-            <ThemeModeProvider>
-                <App />
-            </ThemeModeProvider>
-        </React.StrictMode>
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    <React.StrictMode>
+      <ThemeModeProvider>
+        <App />
+      </ThemeModeProvider>
+    </React.StrictMode>
+  </QueryClientProvider>
 );
