720f73801826c99c55e6f49b831128343d2b1aaa
sjndowair <wodnjsdl1125@naver.com>
2024-10-09 00:50:16 +0900

    fix: dark 모드 수정
    localstorage 롤 페이지 이탈 시에도 마지막에 선택 해둔 테마로 유지되도록 로직 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./components/Theme/theme";
-import { GlobalStyle } from "./components/Theme/global-style";
+import { darkTheme, lightTheme } from "./components/theme/theme";
+import { GlobalStyle } from "./components/theme/global-style";
 import { useThemeMode } from "./contexts/themeCtx";
-import Home from "./pages/root/root";
-import SearchPage from "./pages/search/Search";
-import Series from "./pages/Series";
+import Home from "./pages/root";
+import SearchPage from "./pages/search";
+import Series from "./pages/series";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Detail from "./pages/detail/Detail";
+import Detail from "./pages/detail";
+import { useEffect } from "react";
+
+const THEME_KEY = "THIS_THEME_KEY";
 
 function App() {
-  const { isDark } = useThemeMode();
+  const { isDark, toggleTheme } = useThemeMode();
+
+  useEffect(() => {
+    const isSavedTheme = localStorage.getItem(THEME_KEY);
+    if (isSavedTheme === "dark") toggleTheme();
+  }, [toggleTheme]);
+
+  useEffect(() => {
+    !isDark
+      ? localStorage.setItem(THEME_KEY, "dark")
+      : localStorage.setItem(THEME_KEY, "light");
+  }, [isDark]);
 
   return (
-    <ThemeProvider theme={!isDark ? lightTheme : darkTheme}>
+    <ThemeProvider theme={isDark ? lightTheme : darkTheme}>
       <GlobalStyle />
       <HashRouter basename="/">
         <Routes>
