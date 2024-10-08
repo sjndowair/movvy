16e96b96765afcf0423c380f44b857232e302d47
sjndowair <wodnjsdl1125@naver.com>
2024-10-09 00:49:31 +0900

    fix: root page style themeCtx로 수정

--- src/pages/root/style.ts
+++ src/pages/root/style.ts
@@ -1,29 +1,28 @@
 import styled from "styled-components";
-import { mediaSize } from "../../components/Theme/theme"; // 안쓰는 import 제거
+import { mediaSize } from "../../components/theme/theme"; // 안쓰는 import 제거
 
 export const SortBox = styled.div`
-    display: flex;
-    gap: 2rem;
-    justify-content: flex-start;
+  display: flex;
+  gap: 2rem;
+  justify-content: flex-start;
 `;
 
 export const TemporaryIframe = styled.iframe`
-    position: absolute;
-    z-index: 0;
+  position: absolute;
+  z-index: 0;
 `;
 
 export const CardCollectionBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    top: -10rem;
-    width: 100%;
-    height: auto;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  height: auto;
 `;
 
 export const SubPageContainer = styled.div`
-    background-color: rgba(0, 0, 0, 1);
-    width: 100%;
-    height: 100vw;
-    color: rgba(255, 255, 255, 1);
+  background-color: ${({ theme }) => theme.background.body};
+  width: 100%;
+  height: 100vw;
+  color: ${({ theme }) => theme.color.average};
 `;
