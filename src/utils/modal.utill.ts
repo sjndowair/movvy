48f91fcb616fa5fa7815640571a99e981aac9104
sjndowair <wodnjsdl1125@naver.com>
2024-10-10 19:54:51 +0900

    delete: 사용하지않는 모달 유틸 삭제

--- src/utils/modal.utill.ts
+++
@@ -1,2 +0,0 @@
-export const MODAL_URL =
-  "https://media.istockphoto.com/id/1494881880/ko/%EC%82%AC%EC%A7%84/3d-%EB%84%98%EB%B2%84%EC%9B%90-1-orangebackground.jpg?s=612x612&w=0&k=20&c=WvyIFiL3tjjA49oqsPK4Pb0p3ClH6VOgO_Nu7IKTvoA=";
